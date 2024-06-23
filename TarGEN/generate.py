0830182c13c245667bb8f0971c7f37d0609f20f5
kscaria <scariakevin1@gmail.com>
2024-06-22 22:07:17 -0700

    Refactor experiments to classes

--- TarGEN/generate.py
+++ TarGEN/generate.py
@@ -1,37 +1,30 @@
 import os
 import json
 import time
-import numpy as np
 from tqdm.auto import tqdm
-
-# Type Hints
 from typing import Optional
-
-from langchain.chat_models import ChatOpenAI
-from ._prompt_builder import PromptBuilder
+from langchain_openai import ChatOpenAI
 
 
 class Generate:
     def __init__(self,
                  api_key: str,
-                 n: Optional[int] = None
                  ):
-        self.prompt_builder = PromptBuilder()
         self.model = ChatOpenAI(openai_api_key=api_key)
-        self.n = n or 2
 
     def create_synthetic_data(self,
-                              step1_human_prompt: str,
-                              step2_human_prompt: str,
-                              step3_human_prompt: str,
-                              step4_human_prompt: str,
+                              generator_function,
                               output_path: str = None,
                               n_samples: Optional[int] = 100,
-                              step3_parser: Optional = None
+                              step1_human_prompt: Optional[str] = None,
+                              step2_human_prompt: Optional[str] = None,
+                              step3_human_prompt: Optional[str] = None,
+                              step4_human_prompt: Optional[str] = None,
+                              overwrite: Optional[bool] = False
                               ):
 
         # Dump path
-        if not os.path.exists(output_path):
+        if not os.path.exists(output_path) or overwrite:
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             samples = []
             start_idx = 0
@@ -44,26 +37,13 @@ class Generate:
             start_idx = len(samples)
 
         pbar = tqdm(range(start_idx, n_samples))
-        counter = 0
         start_time = time.time()
-
-        # Build the prompt
-        contexts_prompt = self.prompt_builder.get_contexts(step1_prompt=step1_human_prompt)
-        step1_output = self.model.invoke(input=contexts_prompt)
-        contexts = self.prompt_builder.csv_output_parser.parse(step1_output.content)
-        if "1" in contexts[0]:
-            contexts = self.prompt_builder.numbered_list_parser.parse(step1_output.content)
+        counter = 0
 
         for idx in pbar:
             pbar.set_description(desc=f"{idx + 1}")
-            DOMAIN = np.random.choice(contexts, size=1, replace=True)[0]
-            instance_seeds_prompt = self.prompt_builder.get_instance_seed(step2_prompt=step2_human_prompt,
-                                                                          domain=DOMAIN,
-                                                                          n=self.n
-                                                                          )
-
-            step2_output = self.model.invoke(input=instance_seeds_prompt)
-            instance_seeds = self.prompt_builder.numbered_list_parser.parse(step2_output.content)
+            instance_sample = generator_function(self.model, step1_human_prompt, step2_human_prompt,
+                                                 step3_human_prompt, step4_human_prompt)
 
             # Rate limiter
             counter += 1
@@ -76,15 +56,6 @@ class Generate:
                 counter = 0
                 start_time = time.time()
 
-            INSTANCE_SEED = np.random.choice(instance_seeds, size=1, replace=True)[0]
-            samples_prompt = self.prompt_builder.get_samples(step3_prompt=step3_human_prompt,
-                                                             sentence=INSTANCE_SEED
-                                                             )
-            step3_output = self.model.invoke(input=samples_prompt)
-            instance_sample = step3_parser(step3_output)
-            instance_sample["premise"] = INSTANCE_SEED
-            instance_sample["domain"] = DOMAIN
-
             # Iteratively dump json
             with open(output_path, mode='w', encoding='utf-8') as append_json:
                 samples.append(instance_sample)
@@ -92,6 +63,6 @@ class Generate:
 
             pbar.update(1)
 
-            if len(samples) == n_samples+1:
+            if len(samples) == n_samples + 1:
                 print('Reached target sample count. Terminating..')
                 return samples
--- main.py
+++ main.py
@@ -1,99 +1,28 @@
 import configparser
-
 from TarGEN import Generate
-from experiments.copa import copa_config, custom_copa_parser
+from experiments.copa import SyntheticCopa
+from TarGEN.base_experiments import BaseExperiments
 
 config = configparser.ConfigParser()
 config.read('./config.ini')
 API_KEY = config.get('targen', 'OPEN_AI_KEY')
+TARGET_DATA_STYLE = "COPA"
 
 # Load TarGEN
 targen = Generate(api_key=API_KEY)
 
-# Task 1 - COPA
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
+# Load orchestrator
+if TARGET_DATA_STYLE == "COPA":
+    target_data_orchestrator = SyntheticCopa()
+else:
+    target_data_orchestrator = BaseExperiments()
 
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 2
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 3
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 4
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 5
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 6
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 7
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
-                             )
-
-# Task 8
-step1_human_prompt = copa_config["step1_prompt"]
-step2_human_prompt = copa_config["step2_prompt"]
-step3_human_prompt = copa_config["step3_prompt"]
-step4_human_prompt = copa_config["step4_prompt"]
-
-targen.create_synthetic_data(step1_human_prompt, step2_human_prompt, step3_human_prompt,
-                             step4_human_prompt, n_samples=15, step3_parser=custom_copa_parser,
-                             output_path="./outputs/copa_sample.json"
+targen.create_synthetic_data(generator_function=target_data_orchestrator.generator_function,
+                             output_path="outputs/copa_sample.json",
+                             n_samples=6,
+                             step1_human_prompt=target_data_orchestrator.get_config()["step1_prompt"],
+                             step2_human_prompt=target_data_orchestrator.get_config()["step2_prompt"],
+                             step3_human_prompt=target_data_orchestrator.get_config()["step3_prompt"],
+                             step4_human_prompt=target_data_orchestrator.get_config()["step4_prompt"],
+                             overwrite=True
                              )
